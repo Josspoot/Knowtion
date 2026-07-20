150e4c034b42b56ab6c9deb176b77945c5f24f0f
Josue Enmanuel Poot Mateo <josue.poot@tecdesoftware.edu.mx>
2026-07-20 10:18:51 -0600

    Init: first commit in knowtion

---
+++ src/main/java/com/mx/tecdesoftware/knowtion/KnowtionApplication.java
@@ -0,0 +1,13 @@
+package com.mx.tecdesoftware.knowtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowtionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowtionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mx/tecdesoftware/knowtion/KnowtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mx.tecdesoftware.knowtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowtionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
